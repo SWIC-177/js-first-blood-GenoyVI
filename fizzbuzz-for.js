83dfe703b45501929c9879521b85f3016ad34ad1
Etcetera Zahz <157307429+GenoyVI@users.noreply.github.com>
2024-03-26 15:58:24 -0500

    Updates Made To Loops and Array Order/Notes

--- fizzbuzz-for.js
+++ fizzbuzz-for.js
@@ -1,11 +1,12 @@
 for (let i = 1; i <= 100; i += 1) {
+  // a for loop with initialization, declaration, and increments of 1.
   if (i % 3 === 0 && i % 5 === 0) {
-    console.log("FizzBuzz");
+    console.log("FizzBuzz"); // using the modulo operator; logging to console if i is divisible by 3 and 5 without a remainder
   } else if (i % 3 === 0) {
-    console.log("Fizz");
+    console.log("Fizz"); // funny word
   } else if (i % 5 === 0) {
-    console.log("Buzz");
+    console.log("Buzz"); // another funny word
   } else {
-    console.log(i);
+    console.log(i); // a final else to log all unaccounted for exceptions.
   }
 }
--- fizzbuzz-while.js
+++ fizzbuzz-while.js
@@ -1,8 +1,9 @@
 let number = 1;
 
 while (number <= 100) {
+  // applying to all code below
   if (number % 3 === 0 && number % 5 === 0) {
-    console.log("FizzBuzz");
+    console.log("FizzBuzz"); // if number/multiple is divisible by 3 AND 5, with NO REMAINDER; print FizzBuzz
   } else if (number % 3 === 0) {
     console.log("Fizz");
   } else if (number % 5 === 0) {
@@ -11,5 +12,5 @@ while (number <= 100) {
     console.log(number);
   }
 
-  number += 1;
+  number += 1; // using assigned and declared variable to increment by 1 via a compound assignment operator
 }
