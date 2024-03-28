f18934c12c9388a3a9fe92c115f9852507587110
Etcetera Zahz <157307429+GenoyVI@users.noreply.github.com>
2024-03-28 14:33:28 -0500

    Fixed and Nested Name Array With For Loop and made alternative changes

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
-const person = {
-  name: "John Oliver",
-  age: 18,
-};
+const people = [
+  {
+    name: "brian griffin",
+    age: 17,
+  },
+  {
+    name: "mary smith",
+    age: 20,
+  },
+  {
+    name: "jane smith",
+    age: 25,
+  },
+  {
+    name: "Peter griffin",
+    age: 30,
+  },
+  {
+    name: "stewart griffin",
+    age: 3,
+  },
+  {
+    name: "John Oliver",
+    age: 46,
+  },
+];
 
 function verifyAdulthood(personName, personAge) {
-  if (personAge >= 18) return `Welcome, ${personName}!`;
-
-  return `You are not old enough to enter, ${personName}.`;
+  for (let i = 0; i < people.length; i += 1) {
+    // let due to reassignment
+    if (people[i].name === personName && people[i].age === personAge) {
+      if (personAge >= 18) {
+        return `Welcome, ${personName}!`;
+      }
+      return `You are not old enough to enter, ${personName}.`;
+    }
+  }
+  return `Person not found.`; // else placed for return
 }
 
-const adultMessage = verifyAdulthood(person.name, person.age);
-
-console.log(adultMessage);
+for (let i = 0; i < people.length; i += 1) {
+  const person = people[i]; // let reacts differently upon reassignment
+  const adultMessage = verifyAdulthood(person.name, person.age);
+  console.log(`${person.name}: ${adultMessage}`);
+}
