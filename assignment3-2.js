2a2ddbb6a8f6893f4fdca9d93c04a0a5b1ab0b68
Etcetera Zahz <157307429+GenoyVI@users.noreply.github.com>
2024-04-09 23:33:58 -0500

    created and edited files for assignments

---
+++ assignment3-2.js
@@ -0,0 +1,45 @@
+const people = [
+  {
+    id: 1,
+    name: "John Doe",
+    age: 23,
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    age: 29,
+  },
+  {
+    id: 3,
+    name: "Jim Doe",
+    age: 34,
+  },
+  {
+    id: 4,
+    name: "Jill Doe",
+    age: 45,
+  },
+  {
+    id: 5,
+    name: "Jack Doe",
+    age: 56,
+  },
+  {
+    id: 6,
+    name: "Jenny Doe",
+    age: 67,
+  },
+];
+
+function renameOdds(p) {
+  const royArray = p.map((person) => {
+    if (person.id % 2 !== 0) {
+      return { ...person, name: "Odd Name" };
+    }
+    return person;
+  });
+
+  return royArray;
+}
+
+console.log(renameOdds(people));
